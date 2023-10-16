ca199d119c1b6795b6d5d890ca0566c4b3dce909
KabiPriya <kabipriya.t@gmail.com>
2023-10-17 00:35:52 +0530

    added edit component

---
+++ app/src/components/ToEditList.jsx
@@ -0,0 +1,23 @@
+
+import React, { useState } from "react";
+
+const EditForm = ({ text, onSave }) => {
+  const [editedText, setEditedText] = useState(text);
+
+  const handleSave = () => {
+    onSave(editedText);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={editedText}
+        onChange={(e) => setEditedText(e.target.value)}
+      />
+      <button onClick={handleSave}>Edit</button>
+    </div>
+  );
+};
+
+export default ToEditList;
