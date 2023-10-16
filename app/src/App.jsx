c200bff66a0b1eb3374c65d2bb97a376285dbc94
KabiPriya <kabipriya.t@gmail.com>
2023-10-16 23:24:30 +0530

    first commit

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,35 +1,103 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useReducer } from "react";
+import TodoAddForm from "./components/TodoAddForm";
+import TodoList from "./components/TodoList";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, dispatch] = useReducer(todoReducer, []);
+
+  function todoReducer(todos, action) {
+    switch (action.type) {
+      case "TODO_ADD": {
+        return [
+          ...todos,
+          {
+            id: new Date().getTime(),
+            text: action.value,
+            isDone: false,
+            isEdit: false,
+          },
+        ];
+      }
+      case "TODO_DELETE": {
+        const filtered = todos.filter((t) => t.id != action.value);
+        return [...filtered];
+      }
+      case "TODO_EDIT": {
+        const newTodos = [...todos];
+        const idx = newTodos.findIndex((nt) => nt.id === action.value);
+        if (idx !== -1) {
+          newTodos[idx]["isEdit"] = true;
+        }
+        return newTodos;
+      }
+      case "TODO_DONE": {
+        const newTodos = [...todos];
+        const idx = newTodos.findIndex((nt) => nt.id === action.value);
+        if (idx !== -1) {
+          newTodos[idx]["isDone"] = true;
+        }
+        return newTodos;
+      }
+      case "TODO_UNDONE": {
+        const newTodos = [...todos];
+        const idx = newTodos.findIndex((nt) => nt.id === action.value);
+        if (idx !== -1) {
+          newTodos[idx]["isDone"] = false;
+        }
+        return newTodos;
+      }
+      default: {
+        throw Error("Unknown action: " + action.type);
+      }
+    }
+  }
+
+  function handleAdd(value) {
+    dispatch({
+      type: "TODO_ADD",
+      value: value,
+    });
+  }
+
+  function handleDelete(id) {
+    dispatch({
+      type: "TODO_DELETE",
+      value: id,
+    });
+  }
+  function handleEdit(id) {
+    dispatch({
+      type: "TODO_EDIT",
+      value: id,
+    });
+  }
+
+  function handleDone(id, type) {
+    if (type == "done") {
+      dispatch({
+        type: "TODO_DONE",
+        value: id,
+      });
+    } else {
+      dispatch({
+        type: "TODO_UNDONE",
+        value: id,
+      });
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>My Todo App</h1>
+      <TodoAddForm handleAdd={handleAdd} />
+      <TodoList
+        todos={todos}
+        handleDelete={handleDelete}
+        handleDone={handleDone}
+        handleEdit={handleEdit}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
